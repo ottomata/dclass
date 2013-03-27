3397e8f78d15ccbf8f4ed832b08e79a7b58065e5
Petrea Corneliu Stefan <stefan@garage-coding.com>
2013-03-28 01:05:16 +0200

    stripped down pm of less useful things. started writing XS methods

--- perl/UA-dClass/dClass.xs
+++ perl/UA-dClass/dClass.xs
@@ -6,6 +6,24 @@
 
 #include "const-c.inc"
 
+
+
+
+
 MODULE = UA::dClass		PACKAGE = UA::dClass		
 
-INCLUDE: const-xs.inc
+INCLUDE:    const-xs.inc
+PROTOTYPES: ENABLE
+
+SV*
+classifyUA(self,UA)
+  SV* self
+  SV* UA
+  CODE:
+    STRLEN ua_l;
+    char *ua_buf = SvPV(UA,ua_l);
+    RETVAL = NULL;
+  OUTPUT:
+    RETVAL
+
+
--- perl/UA-dClass/lib/UA/dClass.pm
+++ perl/UA-dClass/lib/UA/dClass.pm
@@ -10,54 +10,11 @@ use AutoLoader;
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use UA::dClass ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
-
-sub AUTOLOAD {
-    # This AUTOLOAD is used to 'autoload' constants from the constant()
-    # XS function.
-
-    my $constname;
-    our $AUTOLOAD;
-    ($constname = $AUTOLOAD) =~ s/.*:://;
-    croak "&UA::dClass::constant not defined" if $constname eq 'constant';
-    my ($error, $val) = constant($constname);
-    if ($error) { croak $error; }
-    {
-	no strict 'refs';
-	# Fixed between 5.005_53 and 5.005_61
-#XXX	if ($] >= 5.00561) {
-#XXX	    *$AUTOLOAD = sub () { $val };
-#XXX	}
-#XXX	else {
-	    *$AUTOLOAD = sub { $val };
-#XXX	}
-    }
-    goto &$AUTOLOAD;
-}
+our $VERSION = '2.02';
 
 require XSLoader;
 XSLoader::load('UA::dClass', $VERSION);
 
-# Preloaded methods go here.
-
-# Autoload methods go after =cut, and are processed by the autosplit program.
 
 1;
 __END__
@@ -65,49 +22,31 @@ __END__
 
 =head1 NAME
 
-UA::dClass - Perl extension for blah blah blah
+UA::dClass - Perl extension Device Detection
 
 =head1 SYNOPSIS
 
+  use strict;
+  use warnings;
   use UA::dClass;
-  blah blah blah
+
+  my $d = UA::dClass->new;
+  my $h = $d->classify("...");
 
 =head1 DESCRIPTION
 
-Stub documentation for UA::dClass, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
-
 =head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
 
 =head1 AUTHOR
 
-user, E<lt>user@E<gt>
+Stefan Petrea, E<lt>stefan@garage-coding.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2013 by user
+Copyright (C) 2013 by Stefan Petrea
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
